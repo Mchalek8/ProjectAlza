15ec1dcf87299ae22f9747fde7b6a1084acb5a74
Pavel Machalek <machap3@medtronic.com>
2021-10-06 15:25:08 +0200

    Update project structure

--- PageObjects/PositionRoot.cs
+++ PageObjects/PositionRoot.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ProjectAlza
 {
-    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
     public class PositionsMeta
     {
         public string href { get; set; }
